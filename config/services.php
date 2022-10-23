5618896c1bc83c01a8610a1aec0256e1047a79ec
Lucas <lucascignacio@gmail.com>
2022-10-23 13:14:37 -0300

    alteracao do redirect google

--- config/services.php
+++ config/services.php
@@ -34,6 +34,6 @@ return [
     'google' => [
         'client_id' => env('GOOGLE_CLIENT_ID'),
         'client_secret' => env('GOOGLE_CLIENT_SECRET'),
-        'redirect' => 'http://127.0.0.1:8001/painel/users',
+        'redirect' => 'http://127.0.0.1:8001/painel',
     ],
 ];
